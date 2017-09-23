2af5a17cd8c0210dc412395aae89f80b95cd2183
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2017-09-23 21:54:03 +0800

    1.1.5

--- build/js/index.js
+++ build/js/index.js
@@ -1,2 +1,33 @@
-webpackJsonp([0],[function(n,e,o){n.exports=o(1)},function(n,e,o){o(2),o(3),document.querySelector(".app-welcome").innerHTML="Welcome to Dawn"},function(n,e){},function(n,e){}],[0]);
+webpackJsonp([0],[
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(1);
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(2);
+
+__webpack_require__(3);
+
+var root = document.querySelector('.app-welcome');
+root.innerHTML = 'Welcome to Dawn';
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+],[0]);
 //# sourceMappingURL=index.js.map
